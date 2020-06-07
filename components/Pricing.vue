2644c4be692852b2db527659bfedaea38267786f
Tom Konidas <tomkonidas@protonmail.ch>
2020-06-07 17:28:23 -0400

    Update contact info

--- components/Pricing.vue
+++ components/Pricing.vue
@@ -6,10 +6,10 @@
                 Pricing
             </h2>
             <p class="mt- text-3xl leading-9 font-extrabold text-white sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none">
-                The right price for you, whoever you are
+                The right price for you, whatever your needs
             </p>
             <p class="mt-3 max-w-4xl mx-auto text-xl leading-7 text-gray-300 sm:mt-5 sm:text-2xl sm:leading-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos odit doloribus molestiae voluptatum.
+                If no solutions meets your needs, please contact us for a custom plan.
             </p>
             </div>
         </div>
