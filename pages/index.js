3ccab46d751902d0457807be32775a438b70dbf6
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-09 20:32:26 -0400

    Cleanup old CSS and add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,20 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Services from "../components/Services";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Sell Your Listings</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <h1>Sell Your Listings</h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
+        <div>
           <Services />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 };
