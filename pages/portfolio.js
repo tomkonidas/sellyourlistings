5b55bcf4e3ddc0feadd944a439b3cad441d904bb
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-15 12:00:49 -0400

    Add layout

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,8 +1,19 @@
+import Head from "next/head";
+import Layout from "../components/Layout";
+import Navbar from "../components/NavBar";
+
 const Portfolio = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      <Head>
+        <title>Portfolio :: Sell Your Listings</title>
+        <meta property="og:title" value="Portfolio :: Sell Your Listings" />
+      </Head>
+      <Layout>
+        <Navbar />
+        <h1>Portfolio</h1>
+      </Layout>
+    </>
   );
 };
 
