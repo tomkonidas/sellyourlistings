d47b12a26b1a4a167c9e63eeceafa6902ac2917b
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-15 12:43:27 -0400

    Add email

--- components/Footer.js
+++ components/Footer.js
@@ -30,6 +30,17 @@ const Footer = () => (
             />
           </svg>
         </a>
+        <a
+          href="mailto:contact@sellyourlistings.ca"
+          className="ml-6 text-gray-100 hover:text-white"
+        >
+          <span className="sr-only">Email</span>
+
+          <svg viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+          </svg>
+        </a>
       </div>
       <div className="mt-8 md:mt-0 md:order-1">
         <p className="text-center text-base leading-6 text-gray-100">
