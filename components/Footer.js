92d867a15a44890675f32a082e2517d6fc094de0
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-15 12:35:14 -0400

    Add branding colors to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 const Footer = () => (
-  <div className="bg-white">
+  <div className="bg-brand">
     <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
       <div className="flex justify-center md:order-2">
         <a
           href="https://www.facebook.com/sellyourlistings-106266777782803"
           rel="noopener noreferrer"
-          className="text-gray-400 hover:text-gray-500"
+          className="text-gray-100 hover:text-white"
         >
           <span className="sr-only">Facebook</span>
           <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -19,7 +19,7 @@ const Footer = () => (
         <a
           href="https://www.instagram.com/sellyourlistings/"
           rel="noopener noreferrer"
-          className="ml-6 text-gray-400 hover:text-gray-500"
+          className="ml-6 text-gray-100 hover:text-white"
         >
           <span className="sr-only">Instagram</span>
           <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -32,7 +32,7 @@ const Footer = () => (
         </a>
       </div>
       <div className="mt-8 md:mt-0 md:order-1">
-        <p className="text-center text-base leading-6 text-gray-400">
+        <p className="text-center text-base leading-6 text-gray-100">
           &copy;{new Date().getFullYear()} Sell Your Listings
         </p>
       </div>
