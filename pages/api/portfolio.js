4d214e8a1761c6d24ad559b46ea2bc9741f0c878
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-15 15:08:22 -0400

    Update data

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -8,5 +8,12 @@ export default (req, res) => {
       matterport: "url",
       youtube: "url",
     },
+    {
+      id: 2,
+      name: "Address 2",
+      showcase: ["img1", "img2", "img3", "img4"],
+      matterport: "url",
+      youtube: "url",
+    },
   ]);
 };
