ded89de686b2f43e51f5dc99856fa21ae328fede
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-16 01:55:27 -0400

    Pricing works

--- components/Service.js
+++ components/Service.js
@@ -1,8 +1,39 @@
-const Service = ({ service }) => {
+import { useState } from "react";
+
+const Service = ({ service, handleServiceToggle }) => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggle = () => {
+    setIsOn(!isOn);
+    handleServiceToggle(service.id);
+  };
+
   return (
-    <div className="py-2 pr-4 pl-2 border-l-8 border-gray-200 hover:border-brand flex justify-between cursor-default hover:bg-gray-100">
+    <div className="py-2 pr-4 pl-2 border-l-8 border-gray-200 hover:border-brand flex items-center justify-between cursor-default hover:bg-gray-100">
       <div>{service.name}</div>
-      <div>{service.price.toFixed(2)}</div>
+      <div className="ml-auto mr-4">{service.price.toFixed(2)}</div>
+
+      <span
+        role="checkbox"
+        tabIndex="0"
+        aria-checked="false"
+        className="group relative inline-flex items-center justify-center flex-shrink-0 h-5 w-10 cursor-pointer focus:outline-none"
+      >
+        <span
+          onClick={handleToggle}
+          aria-hidden="true"
+          className={`bg-gray-200 absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200 ${
+            isOn ? "bg-brand" : "bg-gray-200"
+          }`}
+        ></span>
+        <span
+          onClick={handleToggle}
+          aria-hidden="true"
+          className={`translate-x-0 absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform group-focus:shadow-outline group-focus:border-blue-300 transition-transform ease-in-out duration-200 ${
+            isOn ? "translate-x-5" : "translate-x-0"
+          }`}
+        ></span>
+      </span>
     </div>
   );
 };
--- components/ServicesConfigurator.js
+++ components/ServicesConfigurator.js
@@ -2,24 +2,38 @@ import { useState, useEffect } from "react";
 import Service from "./Service";
 
 const ServicesConfigurator = ({ servicesData, categoriesData }) => {
-  const [services, setServices] = useState(servicesData);
+  const [services, setServices] = useState(
+    servicesData.map((service) => ({ ...service, isAdded: false }))
+  );
   const [categories, setCategories] = useState(categoriesData);
   const [filterOut, setFilterOut] = useState("house");
   const [total, setTotal] = useState(0);
+
+  const handleServiceToggle = (id) => {
+    const service = services.find((service) => service.id === id);
+
+    service.isAdded
+      ? setTotal(total - service.price)
+      : setTotal(total + service.price);
+
+    const changedService = { ...service, isAdded: !service.isAdded };
+    setServices(
+      services.map((service) => (service.id !== id ? service : changedService))
+    );
+  };
 
   return (
     <div className="my-4">
       <div>
         {categories.map((category) => (
-          <div className="mb-4 py-2">
-            <h2 className="cursive text-xl py-2" key={category.id}>
-              {category.name}
-            </h2>
+          <div key={category.id} className="mb-4 py-2">
+            <h2 className="cursive text-xl py-2">{category.name}</h2>
             <div>
               {category.services.map((s) => (
                 <Service
                   key={s}
                   service={services.find((service) => service.id === s)}
+                  handleServiceToggle={handleServiceToggle}
                 />
               ))}
             </div>
@@ -27,8 +41,8 @@ const ServicesConfigurator = ({ servicesData, categoriesData }) => {
         ))}
       </div>
       <div className="border-t-4 border-dotted border-brand py-4 pr-4 flex justify-end items-center">
-        <div className="text-xl cursive">Total</div>
-        <div className="ml-4">{total.toFixed(2)}</div>
+        <div className="text-xl cursive mr-4">Total</div>
+        <div>{total.toFixed(2)}</div>
       </div>
     </div>
   );
