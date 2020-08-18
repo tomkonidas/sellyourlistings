43d9843eb508d4ae1da98921e48d90c666042fa0
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-17 21:08:55 -0400

    Add SEO meta

--- pages/_document.js
+++ pages/_document.js
@@ -11,18 +11,40 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <meta
             name="description"
             content="3D dollhouse, drone and interior videos, property photos and social media management. 
             We offer the most affordable prices to help real estate agents. 
             The areas we operate in are the Greater Montreal, Laval, Quebec, Ottawa and Gatineau."
           />
+          {/* Twitter Card data */}
+          <meta name="twitter:card" content="summary" />
+          {/* <meta name="twitter:site" content="@publisher_handle" /> */}
+          <meta name="twitter:title" content="Sell Your Listings" />
           <meta
-            name="og:description"
+            name="twitter:description"
+            content="3D dollhouse, drone and interior videos, property photos and social media management. 
+            We offer the most affordable prices to help real estate agents. "
+          />
+          {/* <meta name="twitter:creator" content="@author_handle" /> */}
+          {/* Twitter Summary card images must be at least 120x120px
+          <meta name="twitter:image" content="http://www.example.com/image.jpg"> */}
+
+          {/* Open Graph data */}
+          <meta property="og:title" content="Sell Your Listings" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://seellyourlistings.ca" />
+          {/* <meta property="og:image" content="http://example.com/image.jpg" /> */}
+          <meta
+            property="og:description"
             content="3D dollhouse, drone and interior videos, property photos and social media management. 
             We offer the most affordable prices to help real estate agents. 
             The areas we operate in are the Greater Montreal, Laval, Quebec, Ottawa and Gatineau."
           />
+          <meta property="og:site_name" content="Sell Your Listings" />
+          {/* <meta property="fb:admins" content="Facebook numeric ID" /> */}
         </Head>
         <body>
           <Main />
