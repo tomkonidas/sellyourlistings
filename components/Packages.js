541bd0fcf0dd1e3d3b012f39677195841449130c
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-17 20:37:32 -0400

    Update spacing

--- components/Packages.js
+++ components/Packages.js
@@ -5,7 +5,7 @@ const Packages = ({ packages, services }) => {
   return (
     <>
       <SectionTitle>All Packages</SectionTitle>
-      <ul className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <ul className="pt-8 pb-12 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {packages.map((p) => (
           <Package key={p.id} packageData={p} services={services} />
         ))}
