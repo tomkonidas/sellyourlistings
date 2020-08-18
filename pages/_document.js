af7252041c6a6e01268322e785482723cc6c0809
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-18 11:40:43 -0400

    Update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,24 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta
             name="description"
             content="3D dollhouse, drone and interior videos, property photos and social media management. 
