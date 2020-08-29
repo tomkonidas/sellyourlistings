7c72e87e01fb1809a098a5a27272c455d3f7db64
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-29 13:31:08 -0400

    Update

---
+++ src/components/packages.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Container from "./container"
+
+const Packages = () => {
+  //     const data = useStaticQuery(graphql`
+  //     query ApplicationsQuery {
+  //       allPlexusCsv {
+  //         edges {
+  //           node {
+  //             id
+  //             Application
+  //             Month
+  //             Year
+  //             Rating__1_4_
+  //             MicroG_Rating__1_4_
+  //             fields {
+  //               slug
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `);
+
+  return (
+    <Container>
+      <div>teste</div>
+      <div>teste</div>
+      <div>teste</div>
+      <div>teste</div>
+      <div>teste</div>
+      <div>teste</div>
+      <div>teste</div>
+    </Container>
+  )
+}
+
+export default Packages
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import LandingHeader from "../components/landingHeader"
+import Packages from "../components/packages"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="We help you sell your listings faster" />
     <LandingHeader />
+    <Packages />
   </Layout>
 )
 
