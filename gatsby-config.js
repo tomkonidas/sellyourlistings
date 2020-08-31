6c75429a5354095348f65335125335567605c9c3
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-30 20:29:12 -0400

    mover to top

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
     og_image: `https://sellyourlistings/sellyourlistings.jpg`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-176843902-1",
+        head: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -48,12 +55,7 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-176843902-1",
-      },
-    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
