7ea0e99d4179e900fc979226831e5db2ba1b48aa
Tom Konidas <tomkonidas@protonmail.ch>
2020-08-30 21:08:43 -0400

    Add custom 404 close  #9

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <div className="min-h-screen h-100 flex flex-col items-center">
+      <div className="mt-1/6 flex items-center">
+        <h1 className="px-4 cursive text-5xl font-semibold border-brand border-r-4">
+          404
+        </h1>
+        <p className="px-4 text-xl font-medium">
+          This page could not be found.
+        </p>
+      </div>
+    </div>
+  </>
 )
 
 export default NotFoundPage
